0d081a078b00c294e8375fefc49fe18513d4ff11
woojung <woojung@welgram.com>
2022-06-14 19:31:04 +0900

    StreamTest.java

---
+++ src/main/java/_21_strategy/_01_before/Client.java
@@ -0,0 +1,12 @@
+package _21_strategy._01_before;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        BlueLightRedLight blueLightRedLight = new BlueLightRedLight(1);
+        blueLightRedLight.blueLight();
+        blueLightRedLight.redLight();
+    }
+
+}
---
+++ src/main/java/_21_strategy/_02_after/Speed.java
@@ -0,0 +1,8 @@
+package _21_strategy._02_after;
+
+public interface Speed {
+
+    void blueLight();
+
+    void redLight();
+}
