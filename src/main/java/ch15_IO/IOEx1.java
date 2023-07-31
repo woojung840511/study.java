b4a8ea25c219941eb887d665599648235824413c
woojung8405 <woojung8405@gmail.com>
2023-07-31 20:47:50 +0900

    IO 학습

---
+++ src/main/java/ch15_IO/IOEx1.java
@@ -0,0 +1,38 @@
+package ch15_IO;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
+public class IOEx1 {
+
+    public static void main(String[] args) {
+        byte[] inSrc = {0,1,2,3,4,5,6,7,8,};
+        byte[] outSrc = null;
+
+        ByteArrayInputStream input = null;
+        ByteArrayOutputStream output = null;
+
+        input = new ByteArrayInputStream(inSrc);
+        output = new ByteArrayOutputStream();
+
+        int data = 0;
+
+        while ((data = input.read()) != -1) {
+            output.write(data);  // void write(int b)
+        }
+
+        /*
+        바이트배열은 사용하는 자원이 메모리 밖에 없으므로
+        가비지 컬럭테에 의해 자동적으로 자원을 반환하므로 close()를 이용해서 스트림을 닫지 않아도 된다.
+        */
+
+        /*
+
+        */
+        outSrc = output.toByteArray(); // 스트림의 내용을 byte 배열로 반환한다
+
+        System.out.println("Input Source : " + Arrays.toString(inSrc));
+        System.out.println("Output Source : " + Arrays.toString(outSrc));
+    }
+}
