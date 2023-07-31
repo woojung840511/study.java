c2ba4ed734048c0e865107f4ebf8edf295129307
woojung8405 <woojung8405@gmail.com>
2023-07-31 20:47:27 +0900

    Functional interface 학습

---
+++ src/main/java/ch14_LamdaAndStream/myTest/Data.java
@@ -0,0 +1,12 @@
+package ch14_LamdaAndStream.myTest;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Data {
+
+    public int idx;
+    public String name;
+}
