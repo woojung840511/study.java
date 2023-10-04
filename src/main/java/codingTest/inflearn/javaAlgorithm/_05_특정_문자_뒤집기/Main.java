b93ba985a12341bde4f0dacddabacb3526a6eb0d
woojung8405 <woojung8405@gmail.com>
2023-10-05 01:08:57 +0900

    인프런 자바코딩테스트 문제 풀기: 5. 특정 단어 뒤집기

---
+++ src/main/java/codingTest/inflearn/javaAlgorithm/_05_특정_문자_뒤집기/Main.java
@@ -0,0 +1,33 @@
+package codingTest.inflearn.javaAlgorithm._05_특정_문자_뒤집기;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public String solution(String str) {
+        char[] charArray = str.toCharArray();
+        int lt = 0, rt = charArray.length - 1;
+        while (lt < rt) {
+            if (!Character.isAlphabetic(charArray[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(charArray[rt])) {
+                rt--;
+            } else {
+                char temp = charArray[lt];
+                charArray[lt] = charArray[rt];
+                charArray[rt] = temp;
+                lt++;
+                rt--;
+            }
+        }
+
+        return String.valueOf(charArray);
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        System.out.println(new Main().solution(str));
+
+    }
+}
