b79b2dbe9b93830fc8bfa9e4753d90382874c499
우정 <woojung8405@gmail.com>
2023-10-10 14:24:50 +0900

    프로그래머스 level 0 문제 1,2,3

---
+++ src/main/java/algorithm/programmers/Level0/_01_문자열_출력하기/Solution.java
@@ -0,0 +1,11 @@
+package algorithm.programmers.Level0._01_문자열_출력하기;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
---
+++ src/main/java/algorithm/programmers/Level0/_02_a와_b_출력하기/Solution.java
@@ -0,0 +1,14 @@
+package algorithm.programmers.Level0._02_a와_b_출력하기;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
