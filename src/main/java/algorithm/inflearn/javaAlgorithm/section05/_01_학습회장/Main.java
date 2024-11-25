08d0eb07917de2cb4eb40fb16c1a23a22a14f186
woojung8405 <woojung8405@gmail.com>
2024-11-25 16:51:59 +0900

    인프런 알고리즘 : hashMap 관련 문제인 학급회장 문제 (풀이)

--- src/main/java/algorithm/inflearn/javaAlgorithm/section05/_01_학습회장/Main.java
+++ src/main/java/algorithm/inflearn/javaAlgorithm/section05/_01_학습회장/Main.java
@@ -1,57 +1,34 @@
 package algorithm.inflearn.javaAlgorithm.section05._01_학습회장;
 
-import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Scanner;
+import java.util.*;
 
-/**
- * <설명>
- * 학급 회장을 뽑는데 후보로 기호 A, B, C, D, E 후보가 등록을 했습니다.
- * 투표용지에는 반 학생들이 자기가 선택한 후보의 기호(알파벳)가 쓰여져 있으며 선생님은 그 기호를 발표하고 있습니다.
- * 선생님의 발표가 끝난 후 어떤 기호의 후보가 학급 회장이 되었는지 출력하는 프로그램을 작성하세요.
- * 반드시 한 명의 학급회장이 선출되도록 투표결과가 나왔다고 가정합니다.
- *
- * <입력>
- * 첫 줄에는 반 학생수 N(5<=N<=50)이 주어집니다.
- * 두 번째 줄에 N개의 투표용지에 쓰여져 있던 각 후보의 기호가 선생님이 발표한 순서대로 문자열로 입력됩니다.
- *
- * <출력>
- * 학급 회장으로 선택된 기호를 출력합니다.
- *
- * 예시 입력: BACBACCACCBDEDE
- * 예시 출력: C
- */
-public class Main {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int input1 = in.nextInt();
-        String input2 = in.nextLine();
-        System.out.println(solution(input1, input2));
-        return ;
-    }
-
-    private static char solution(int n, String s) {
-        char result = ' ';
-        int max = 0;
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (Character c : s.toCharArray()) {
-            map.computeIfPresent(c, (k, v) -> ++v);
-            map.computeIfAbsent(c, k -> 1);
+class Main {
+    public char solution(int n, String s){
+        char answer=' ';
+        HashMap<Character, Integer> map=new HashMap<>();
+        for(char x : s.toCharArray()){
+            map.put(x, map.getOrDefault(x, 0)+1);
         }
+        //System.out.println(map.containsKey('F'));
+        //System.out.println(map.size());
+        //System.out.println(map.remove('C'));
 
-        for (Integer value : map.values()) {
-            max = Math.max(max, value);
-        }
-
-        for (Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == max) {
-                result = entry.getKey();
+        int max=Integer.MIN_VALUE;
+        for(char key : map.keySet()){
+            //System.out.println(key+" "+map.get(key));
+            if(map.get(key)>max){
+                max=map.get(key);
+                answer=key;
             }
         }
-
-        return result;
+        return answer;
     }
 
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        String str=kb.next();
+        System.out.println(T.solution(n, str));
+    }
 }
